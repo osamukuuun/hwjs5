de47192f8bdfcddebc2bccb2ab5ff64184c06411
osamukuuun <eva2002guk@gmail.com>
2022-10-23 14:17:14 +0300

    first commit

---
+++ js.js
@@ -0,0 +1,44 @@
+// // 1
+
+// let arr = ['апельсин', 'банан'];
+// arr.push("яблуко","диня");
+// console.log(arr);
+
+// // 2
+
+// let arr1 = ['first','second'];
+// let arr2 = [3,4];
+// let all = arr1.concat(arr2);
+// console.log(all);
+
+// 3
+
+// let arr = ['first','second','third'];
+// arr.shift();
+// console.log(arr);  
+
+// 4
+
+// let arr = [1,7,6,4];
+// if(arr.includes(7)){
+//     console.log('Такий елемент існує');
+// }
+// else{
+//  console.log('Такий елемент не існує');
+// }
+
+// 5
+
+// let arr = [];
+// let elem;
+// do{
+//     elem = +prompt("Element : ");
+//     if(arr.includes(elem) || elem == ''|| elem == null){
+//         continue;
+//     }
+//     else{
+//         arr.push(elem);
+//     }
+// }
+// while(elem);
+// console.log("Array without duplicats" + arr);
